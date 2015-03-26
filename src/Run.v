df8bf45fff8b32e8ce990777c7ffda119db7f5a0
Guillaume Claret <dev@clarus.me>
2015-03-26 15:50:55 +0100

    Generalized version of the function exception

--- src/Run.v
+++ src/Run.v
@@ -15,9 +15,9 @@ Fixpoint lift {E1 E2 : Effect.t} {A : Type}
   | C.First _ _ x y => C.First _ _ (lift run x) (lift run y)
   end.
 
-Fixpoint exception {E : Effect.t} {Exc A : Type}
-  (run : forall (c : Effect.command E), C.t E (Effect.answer E c + Exc))
-  (run_join : Exc -> Exc -> Exc) (x : C.t E A) : C.t E (A + Exc) :=
+Fixpoint exception {E1 E2 : Effect.t} {Exc A : Type}
+  (run : forall (c : Effect.command E1), C.t E2 (Effect.answer E1 c + Exc))
+  (run_join : Exc -> Exc -> Exc) (x : C.t E1 A) : C.t E2 (A + Exc) :=
   match x with
   | C.Ret _ x => ret @@ inl x
   | C.Call c => run c
